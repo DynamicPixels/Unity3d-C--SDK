e181ef09081222e70916205f1c52f6c8ea4e89b9
Amirfery <Amirferyg@gmail.com>
2024-10-21 19:05:04 +0330

    fix: fix RealtimeService.cs

--- Runtime/Services/MultiPlayer/Realtime/RealtimeService.cs
+++ Runtime/Services/MultiPlayer/Realtime/RealtimeService.cs
@@ -1,14 +1,14 @@
 using System.Threading.Tasks;
 using DynamicPixels.GameService.Models;
-using DynamicPixels.GameService.Utils.WebsocketClient;
+using DynamicPixels.GameService.ModuleFramework.Messaging;
 
 namespace DynamicPixels.GameService.Services.MultiPlayer.Realtime
 {
     public class RealtimeService : IRealtimeService
     {
-        private readonly ISocketAgent _socketAgent;
+        private readonly IWebSocketService _socketAgent;
 
-        public RealtimeService(ISocketAgent socketAgent)
+        public RealtimeService(IWebSocketService socketAgent)
         {
             _socketAgent = socketAgent;
         }
@@ -21,7 +21,7 @@ namespace DynamicPixels.GameService.Services.MultiPlayer.Realtime
                 Payload = message
             };
 
-            return _socketAgent.Send(packet);
+            return _socketAgent.SendAsync(packet);
         }
     }
 }
