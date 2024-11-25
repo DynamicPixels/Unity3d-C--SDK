a0e28da380e086b981910a5c4931cb04cd667c9a
Amirfery <Amirferyg@gmail.com>
2024-11-25 19:08:46 +0330

    chore: remove realtime folder

--- Runtime/Services/MultiPlayer/Realtime/IRealtimeService.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace DynamicPixels.GameService.Services.MultiPlayer.Realtime
-{
-    public interface IRealtimeService
-    {
-        Task SendMessage(int receiverId, string message);
-    }
-}
